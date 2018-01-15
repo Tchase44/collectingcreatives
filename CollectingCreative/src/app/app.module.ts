5fd7dac3908dc2571bd9c2ff2831767aefe385e0
Tchase44 <tchase4544@gmail.com>
2018-01-15 17:48:56 -0500

    sntax fix

--- CollectingCreative/src/app/app.module.ts
+++ CollectingCreative/src/app/app.module.ts
@@ -5,9 +5,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LocationsComponent } from './locations/locations.component';
 import { LocationsAllComponent } from './locations-all/locations-all.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
-// ng g module app-routing --flat --module=app
+
 
 @NgModule({
   declarations: [
