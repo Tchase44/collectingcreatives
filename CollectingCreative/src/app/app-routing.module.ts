810d76cbae96d7152fc4253388c63c2564b6a93a
Tchase44 <tchase4544@gmail.com>
2018-01-16 11:04:40 -0500

    linting fixes in router

--- CollectingCreative/src/app/app-routing.module.ts
+++ CollectingCreative/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-//import { CommonModule } from '@angular/common';
+// import { CommonModule } from '@angular/common';
 
 import { LocationsComponent } from './locations/locations.component';
 import { LocationsAllComponent } from './locations-all/locations-all.component';
@@ -7,15 +7,16 @@ import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes =[
+const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
   {path: 'locations', component: LocationsComponent},
   {path: 'allLocations', component: LocationsAllComponent},
-  {path: 'home', component: HomeComponent}
- // {path: '', component:''},
-  //{path: '', component:''},
-  //{path: '', component:''}
-]
+  // {path: 'appearances', component:},
+  // {path: 'allappearances', component:},
+  // {path: 'videos', component:}
+  // {path: 'allvideos', component:}
+];
 
 
 @NgModule({
