19685320f97da9ad23fc6ed4329800660c711a59
Tchase44 <tchase4544@gmail.com>
2018-01-16 19:11:33 -0500

    added routes

--- CollectingCreative/src/app/app-routing.module.ts
+++ CollectingCreative/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 
 import { RouterModule, Routes } from '@angular/router';
-//import { CommonModule } from '@angular/common';
+// import { CommonModule } from '@angular/common';
 
 
 import { AppComponent } from './app.component';
 import { LocationsComponent } from './locations/locations.component';
 import { LocationsAllComponent } from './locations-all/locations-all.component';
 import { HomeComponent } from './home/home.component';
+import { AppearancesComponent } from './appearances/appearances.component';
+import { VideosComponent } from './videos/videos.component';
+import { VideolistComponent } from './Videos/videolist/videolist.component';
 
 
 const routes: Routes = [
@@ -15,10 +18,10 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'locations', component: LocationsComponent},
   {path: 'allLocations', component: LocationsAllComponent},
-  // {path: 'appearances', component:},
+  {path: 'appearances', component: AppearancesComponent},
   // {path: 'allappearances', component:},
-  // {path: 'videos', component:}
-  // {path: 'allvideos', component:}
+  {path: 'videos', component: VideosComponent}
+  {path: 'allvideos', component: VideolistComponent}
 ];
 
 
