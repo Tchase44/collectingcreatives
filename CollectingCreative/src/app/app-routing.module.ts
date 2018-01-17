526f6b6c575ed47ee19244967bf72bf717d58f33
baopham92 <baopham92@yahoo.com>
2018-01-16 23:53:25 -0500

    subtle css changes

--- CollectingCreative/src/app/app-routing.module.ts
+++ CollectingCreative/src/app/app-routing.module.ts
@@ -14,14 +14,14 @@ import { VideolistComponent } from './Videos/videolist/videolist.component';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'locations', component: LocationsComponent},
-  {path: 'allLocations', component: LocationsAllComponent},
-  {path: 'appearances', component: AppearancesComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'locations', component: LocationsComponent },
+  { path: 'allLocations', component: LocationsAllComponent },
+  { path: 'appearances', component: AppearancesComponent },
   // {path: 'allappearances', component:},
-  {path: 'videos', component: VideosComponent},
-  {path: 'allvideos', component: VideolistComponent}
+  { path: 'videos', component: VideosComponent },
+  { path: 'allvideos', component: VideolistComponent }
 ];
 
 
@@ -29,7 +29,7 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes)
   ],
-  exports:[ RouterModule ],
+  exports: [RouterModule],
   declarations: []
 })
 export class AppRoutingModule { }
