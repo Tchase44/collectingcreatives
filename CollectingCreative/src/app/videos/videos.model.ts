3e21bc5c8e9c52e0f9928ac12e27a31ed8040fd1
baopham92 <baopham92@yahoo.com>
2018-01-18 19:26:39 -0500

    video model added

---
+++ CollectingCreative/src/app/videos/videos.model.ts
@@ -0,0 +1,11 @@
+export class Videos {
+    public name: string;
+    public description: string;
+    public link: string;
+
+    constructor( name: string, description: string, link: string) {
+    this.name = name;
+    this.description = description;
+    this.link = link;
+    }
+}
