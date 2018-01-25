33f3b38e64507948e9bfd87f5fee0ca862d1d9f4
Tchase44 <tchase4544@gmail.com>
2018-01-25 16:46:40 -0500

    env testing

--- CollectingCreative/server.js
+++ CollectingCreative/server.js
@@ -4,26 +4,18 @@ const path = require('path');
 const http = require('http');
 const app = express();
 
+require('dotenv').config()
 //Set Port
 // const port = process.env.PORT || '3000';
 // app.set('port', port);
 app.set("port", process.env.PORT || 8000)
 
-// API file for interacting with MongoDB
-// optional we can create API routes on this page if necessary
-const api = require('./server/routes/api');
-
-// Parsers
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Angular DIST output folder
 // dist/ folder will not exsit until angular runs a build process
 app.use(express.static(path.join(__dirname, 'dist')));
-
-// API location
-// not sure what this code does needs to be tested
-app.use('/api', api);
 
 // Sends ALL requests to the Angular app, will render the app in browser
 app.get('*', (req, res) => {
