eb21843efdbfcc4f91d14fc225b7c996ad03d5d3
Tchase44 <tchase4544@gmail.com>
2018-01-29 19:09:06 -0500

    http refernce lookup

--- CollectingCreative/src/app/app.module.ts
+++ CollectingCreative/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import {} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LocationsComponent } from './locations/locations.component';
