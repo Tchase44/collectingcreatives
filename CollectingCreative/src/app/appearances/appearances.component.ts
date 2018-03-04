f7328f74e4e1602062822e8ea0809d78bed60f41
baopham92 <baopham92@yahoo.com>
2018-03-03 22:55:56 -0500

    imported layout to locations

--- CollectingCreative/src/app/appearances/appearances.component.ts
+++ CollectingCreative/src/app/appearances/appearances.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Photos } from '../mock-images';
 @Component({
   selector: 'app-appearances',
+  encapsulation: ViewEncapsulation.Native,
   templateUrl: './appearances.component.html',
   styleUrls: ['./appearances.component.css']
 })
--- CollectingCreative/src/app/locations/locations.component.ts
+++ CollectingCreative/src/app/locations/locations.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { trigger, style, animate, transition } from '@angular/animations';
 import { Photos } from '../mock-images';
 // import { NgForOf } from '@angular/common';
@@ -6,6 +6,7 @@ import { Http, Response, RequestOptions, Headers } from '@angular/http';
 
 @Component({
   selector: 'app-locations',
+  encapsulation: ViewEncapsulation.Native,
   templateUrl: './locations.component.html',
   styleUrls: ['./locations.component.css'],
   animations: []
